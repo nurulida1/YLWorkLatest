683276ded7669937ffc3e3e5387dcf1ece40b903
nurulida1 <nurulida0102@gmail.com>
2026-07-23 14:45:16 +0800

    update reponsive layout

--- frontend/src/app/common/components/ResponsiveLayout/ResponsiveLayout.ts
+++ frontend/src/app/common/components/ResponsiveLayout/ResponsiveLayout.ts
@@ -1,29 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map } from 'rxjs/operators';
+
 import { WebLayout } from '../../../shared/components/web-layout/web-layout';
 import { MobileLayout } from '../../../shared/components/mobile-layout/mobile-layout';
-import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-responsive-layout',
+  standalone: true,
   imports: [CommonModule, WebLayout, MobileLayout],
-  template: ` <app-mobile-layout *ngIf="isMobile; else desktop" />
+  template: `
+    <app-mobile-layout *ngIf="isMobile$ | async; else desktop" />
+
     <ng-template #desktop>
       <app-web-layout />
-    </ng-template>`,
+    </ng-template>
+  `,
   styleUrl: './ResponsiveLayout.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResponsiveLayout {
   private breakpointObserver = inject(BreakpointObserver);
 
-  isMobile = false;
-
-  constructor() {
-    this.breakpointObserver
-      .observe('(max-width: 768px)')
-      .subscribe((result) => {
-        this.isMobile = result.matches;
-      });
-  }
+  isMobile$ = this.breakpointObserver
+    .observe('(max-width: 768px)')
+    .pipe(map((result) => result.matches));
 }

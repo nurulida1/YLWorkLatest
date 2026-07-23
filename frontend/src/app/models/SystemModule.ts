0110a2cb698625ab2ab251eec1458a05f78239e4
CUKYY <syukri12099@gmail.com>
2026-07-23 10:01:05 +0800

    system module

--- frontend/src/app/models/SystemModule.ts
+++ frontend/src/app/models/SystemModule.ts
@@ -3,11 +3,13 @@ import { BaseModel } from './BaseModel';
 export interface SystemModuleDto extends BaseModel {
   name: string;
   code: string;
+  routePrefix?: string | null;
 }
 
 export interface CreateSystemModuleRequest {
   name: string;
   code: string | null;
+  routePrefix?: string | null;
 }
 
 export interface UpdateSystemModuleRequest extends CreateSystemModuleRequest {
